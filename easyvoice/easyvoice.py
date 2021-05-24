703b11b37c624ec7630d92cfe12b296ca255a29e
Daniil Maksymenko <konor00792@gmail.com>
2021-05-25 00:49:10 +0300

    Add files via upload

--- easyvoice/easyvoice.py
+++ easyvoice/easyvoice.py
@@ -1,4 +1,5 @@
 import base64
+import codecs
 import os
 import requests
 
@@ -38,19 +39,25 @@ class EasyVoice:
         for u in organization_info.get('users'):
             self._members.append(User(u))
 
-    def to_base64(file):
+    def to_base64(self, file):
         with open(file, 'rb') as f:
-            return base64.b64encode(f)
+            return str(base64.b64encode(f.read()))[2:-1]
     
     def add_voice(self, name, desciption, file):
         data = requests.post(
             self.__BASE_URL + 'api/voice/',
-            data={
+            json={
                 'name': name,
                 'description': desciption,
                 'file': self.to_base64(file)
+            },
+            headers={
+                'Authorization': self.__api_key
             }
         ).json()
+
+        self.__voice_fetch_datetime = None
+
         return self.get_voices()
 
     def timedelta(self, date):
@@ -67,9 +74,8 @@ class EasyVoice:
             ).json()
             self.voices = []
             for v in voices_info:
-                self.voices.append(Voice(v))
-            
-            self.voices = list(set(self.voices))
+                if (v.get('public_id') not in [_.public_id for _ in self.voices]):
+                    self.voices.append(Voice(v))
             
             self.__voice_fetch_datetime = datetime.utcnow()
             
@@ -78,7 +84,7 @@ class EasyVoice:
     def get_voicing_queries(self):
         if self.__query_fetch_datetime is None or self.timedelta(self.__query_fetch_datetime):
             queries_info = requests.get(
-                self.__BASE_URL + f'api/query/?organization={self.public_id}',
+                self.__BASE_URL + f'api/query/?organization={self._public_id}',
                 headers={
                     'Authorization': self.__api_key
                 }
@@ -92,10 +98,38 @@ class EasyVoice:
         return self.queries
 
     def voice_text(self, text, voice):
-        pass
+        data = requests.post(
+            self.__BASE_URL + 'api/query/',
+            json={
+                'text': text,
+                'organization': self._public_id,
+                'voice': voice.public_id
+            },
+            headers={
+                'Authorization': self.__api_key
+            }
+        ).json()
+
+        self.__query_fetch_datetime = None
+
+        return self.get_voicing_queries()
 
     def voice_text_file(self, file, voice):
-        pass
-    
-    def download_query_result(self, public_id):
-        pass
+        with codecs.open(file, 'r', encoding='utf-8') as f:
+            text = f.read()
+        
+        data = requests.post(
+            self.__BASE_URL + 'api/query/',
+            json={
+                'text': text,
+                'organization': self._public_id,
+                'voice': voice.public_id
+            },
+            headers={
+                'Authorization': self.__api_key
+            }
+        ).json()
+
+        self.__query_fetch_datetime = None
+
+        return self.get_voicing_queries()
--- easyvoice/query.py
+++ easyvoice/query.py
@@ -1,3 +1,4 @@
+import requests
 from dateutil.parser import *
 
 
@@ -6,6 +7,13 @@ class Query:
         self.text = obj.get('text')
         self.language = obj.get('lang')
         self.date = parse(obj.get('date'))
+        self.time_processed = obj.get('time_processed')
+        self.voice = obj.get('voice')
+        self.url = obj.get('url')
 
-    def download(self):
-        pass
+    def download(self, local_filename):
+        with requests.get(self.url, stream=True) as r:
+            r.raise_for_status()
+            with open(local_filename, 'wb') as f:
+                for chunk in r.iter_content(chunk_size=8192): 
+                    f.write(chunk)
